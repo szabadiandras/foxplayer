a1d9104ec95538c2268be7ceda8fa0595ff36716
szabadiandras <szabadiandras93@gmail.com>
2019-11-25 12:17:31 +0100

    connection added

---
+++ server.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const mysql = require("mysql");
+const express = require("express");
+const path = require("path");
+const PORT = 8080;
+const bodyParser = require("body-parser");
+
+let conn = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: "greenfox",
+  database: "urlAliaserDb"
+});
+
+conn.connect(function(err) {
+  if (err) {
+    console.log("Error connecting to Db");
+    console.log(err);
+    return;
+  }
+  console.log("Connection to database established\n");
+});
+
+let app = express();
+app.use(express.json());
+app.use(express.static("views"));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// END OF REQs & CONNs
+
+
+
+// GET MAIN PAGE
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "./views/index.html"));
+});
+
+// PORT
+
+app.listen(PORT, () => {
+  console.log(`\nServer is running\nListening on port ${PORT}`);
+});

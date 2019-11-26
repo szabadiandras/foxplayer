a1778cfcce615193f6d05765dc7b6d559a96b7cc
szabadiandras <szabadiandras93@gmail.com>
2019-11-26 11:50:40 +0100

    improvements to css & adding js eventlisteners

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,38 @@
-'use strict';
+"use strict";
 
-let playstate = document.querySelector('.playstate');
+let playstate = document.querySelector(".playstate");
 
-playstate.addEventListener('click', function(e) {
-  console.log('play clicked');
-  playstate.classList.remove("play")
-  playstate.classList.add("pause");
+playstate.addEventListener("click", function(e) {
+  console.log("Play/Pause button clicked.");
+
+  if (playstate.className.match("play")) {
+    playstate.className = "paused";
+    playstate.classList.add("pause");
+    playstate.classList.remove("play");
+  } else {
+    playstate.className = "play";
+    playstate.classList.add("play");
+    playstate.classList.remove("paused");
+  }
+});
+
+let volume = document.querySelector(".volume");
+let volumeslider = document.querySelector("#volumeSlider");
+
+volume.addEventListener("click", function(e) {
+  console.log("Volume button clicked.");
+
+  if (volume.className.match("loud")) {
+    volume.className = "muted";
+    volume.classList.add("muted");
+    volume.classList.remove("loud");
+    volumeslider.disabled = true;
+    volumeslider.classList.add("disable");
+  } else {
+    volume.className = "loud";
+    volume.classList.add("loud");
+    volume.classList.remove("paused");
+    volumeslider.disabled = false;
+    volumeslider.classList.remove("disable");
+  }
 });
